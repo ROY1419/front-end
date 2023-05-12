24cf8cee478c4771656c16a28ede6e355c0475ce
Roy1419 <royshubham471@gmail.com>
2023-05-12 17:32:46 +0530

    RT-Weather-app

---
+++ src/helpers/API.jsx
@@ -0,0 +1,5 @@
+export const weatherAppAPI = "981efadb369d0a12f795e182e76d07b6";
+// export const weatherAppAPI = "c0d290eeee9dd399b017a6d2ba64be7e";
+
+// export const googleMapAPI = "AIzaSyAq15HbfCRMW7RqNb5LUNyOLyfzpYI0wl4";
+export const googleMapAPI = "AIzaSyBYCX3xsfq2qeCr7_-XOCzvvUfQ0NEXpAY";
